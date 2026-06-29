21776364a01037389428b6faf6f20f23c90927ba
pmeglaw <patrick@megeredchianlaw.com>
2026-06-29 09:00:21 -0700

    Allow floor plan image query in localPatterns

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: __dirname
+  outputFileTracingRoot: __dirname,
+  images: {
+    localPatterns: [
+      {
+        pathname: "/images/office-floor-plan.png",
+        search: "?v=map-v2-1911x867"
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
